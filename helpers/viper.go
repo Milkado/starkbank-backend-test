8ba5505e721669462c202871ed7b7ff4ec437fc2
Lucas Miranda <lucasdef.miranda@gmail.com>
2026-03-06 16:45:45 -0300

    update action

--- helpers/viper.go
+++ helpers/viper.go
@@ -11,9 +11,17 @@ func Env(key string) string {
 	viper.SetConfigType("env")
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("..")
+	viper.AutomaticEnv() 
 
 	err := viper.ReadInConfig()
 	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			val := viper.GetString(key)
+			if val == "" {
+				fmt.Printf("Warning: .env file not found and key '%s' is not set in environment\n", key)
+			}
+			return val
+		}
 		fmt.Println("error getting key from .env: ", err.Error())
 		panic("viper error")
 	}
